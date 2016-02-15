55a3630444dda717c8230d157bcb5e089d2a9976
Ryder Bright <ryderbright@gmail.com>
2016-02-15 15:48:44 -0800

    models linked

--- views/show.rb
+++ views/show.rb
@@ -2,8 +2,12 @@
 <%= @food.order %>
 <%= @food.table_number %>
 
+  var x = 1
+      1 = x
 
-<form action="/food/<%= @food.id %>" method="post" class='delete'>
+  <form action="/food/<%= @food.id %>" method="get" class= "input">
   <input type="button" name="_method" value="get">
   <input type="submit" value="post">
 </form>
+  p "/food/<%= @food.id %>"  
+
